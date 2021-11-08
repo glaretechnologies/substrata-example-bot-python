e92ca0b732d88038fea4f453980fc404ede603ea
Ono-Sendai <nick@indigorenderer.com>
2021-11-09 02:15:54 +1300

    Added logging in, and updating the transform of an object regularly.

--- substrata_bot_demo.py
+++ substrata_bot_demo.py
@@ -1,5 +1,5 @@
 
-import socket, ssl, pprint, time, select, struct
+import socket, ssl, pprint, time, select, struct, math, configparser
 
 
 CyberspaceHello = 1357924680
@@ -14,10 +14,13 @@ AvatarTransformUpdate	= 1002
 
 ChatMessageID			= 2000
 
+ObjectTransformUpdate	= 3002
 ParcelCreated			= 3100
 
 InfoMessageID			= 7001
 ErrorMessageID			= 7002
+
+LogInMessage			= 8000
 
 TimeSyncMessage			= 9000
 
@@ -82,13 +85,63 @@ class BufferIn:
 		return string_bytes.decode('UTF-8')
 
 
+class BufferOut:
+	def __init__(self):
+		self.data = []
+
+	def writeUInt32(self, x):
+		b = x.to_bytes(4, byteorder='little')
+		#self.data.append(b)
+		self.data += b
+
+	def writeUInt64(self, x):
+		b = x.to_bytes(8, byteorder='little')
+		self.data += b
+
+	def writeFloat(self, x):
+		b = struct.pack("<f", x) # '<' specifies little-endian byte order.
+		self.data += b
+
+	def writeDouble(self, x):
+		b = struct.pack("<d", x) # '<' specifies little-endian byte order.
+		self.data += b
+
+	def writeStringLengthFirst(self, str):
+		b = bytes(str, 'UTF-8')
+		self.writeUInt32(len(b))
+		self.data += b
+
+	def updateLengthField(self):
+		data_len = len(self.data)
+		b = data_len.to_bytes(4, byteorder='little')
+
+		self.data[4 + 0] = b[0]
+		self.data[4 + 1] = b[1]
+		self.data[4 + 2] = b[2]
+		self.data[4 + 3] = b[3]
+
+	def writeToSocket(self, socket):
+		socket.sendall(bytes(self.data))
+
+
+
+
+# Read username and password from disk
+config = configparser.ConfigParser()
+config.read('config.txt')
+username = config['credentials']['username']
+password = config['credentials']['password']
+
+
+print("Using username '" + username + "'")
+
 
 plain_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 conn = ssl.wrap_socket(plain_socket)
 conn.connect(('test.substrata.info',  7600))
 
-print("Connected.")
+print("Connected to server.")
 
 writeUInt32ToSocket(conn, CyberspaceHello)
 writeUInt32ToSocket(conn, CyberspaceProtocolVersion)
@@ -113,10 +166,20 @@ else:
 client_avatar_UID = readUID(conn)
 
 
+# Send login message
+login_buf = BufferOut()
+login_buf.writeUInt32(LogInMessage)
+login_buf.writeUInt32(0) # will be updated with length
+login_buf.writeStringLengthFirst(username)
+login_buf.writeStringLengthFirst(password)
+login_buf.updateLengthField()
+login_buf.writeToSocket(conn)
+
 last_send_time = -1000.0
+initial_time = time.monotonic()
 
 while(1):
-	while(socketReadable(conn, 0.1)): # timeout_ = 0.1
+	while(socketReadable(conn, 0.01)): # timeout_ = 0.1
 		# print("Socket readable!")
 		# Read and handle message(s)
 		msg_type = readUInt32FromSocket(conn)
@@ -133,35 +196,61 @@ while(1):
 			
 			global_time = buffer_in.readDouble()
 
-			print("Received TimeSyncMessage: global_time: " + str(global_time))
+			#print("Received TimeSyncMessage: global_time: " + str(global_time))
 		elif(msg_type == ChatMessageID):
 		
 			name = buffer_in.readStringLengthFirst()
 			msg  = buffer_in.readStringLengthFirst()
 
-			print("Received ChatMessage: '" + name + "': '" + msg +"'")
+			#print("Received ChatMessage: '" + name + "': '" + msg +"'")
 		elif(msg_type == AvatarTransformUpdate):
-			print("Received AvatarTransformUpdate")
+			#print("Received AvatarTransformUpdate")
+			pass
 		elif(msg_type == ParcelCreated):
-			print("Received ParcelCreated")
+			#print("Received ParcelCreated")
+			pass
 		elif(msg_type == InfoMessageID):
 			msg = buffer_in.readStringLengthFirst()
-			print("Received InfoMessage: " + msg)
+			#print("Received InfoMessage: " + msg)
 		elif(msg_type == ErrorMessageID):
 			msg = buffer_in.readStringLengthFirst()
 			print("Received ErrorMessage: " + msg)
 		else:
-			print("Received msg, type: " + str(msg_type) + ", len: " + str(msg_len))
+			#print("Received msg, type: " + str(msg_type) + ", len: " + str(msg_len))
+			pass
 
 	else: # Else if socket was not readable:
-		time.sleep(0.1)
+		
 
-		if(time.monotonic() - last_send_time > 2.0):
+		if(time.monotonic() - last_send_time > 0.1):
 
 			# Send a message to the server
 			last_send_time = time.monotonic()
 
-			print("TODO: Sending a message to server...")
+			# print("Sending message to server...")
+
+			buffer_out = BufferOut()
+			buffer_out.writeUInt32(ObjectTransformUpdate)
+			buffer_out.writeUInt32(0) # will be updated with length
+			buffer_out.writeUInt64(152719) # Write object UID
+
+			buffer_out.writeDouble(12.12375831604) # Write object pos x
+			buffer_out.writeDouble(51.971897125244)# + (time.monotonic() - initial_time)) # Write object pos y
+			buffer_out.writeDouble(1.063417524099)# + math.sin(time.monotonic()) + 1.0) # Write object pos z
+
+			buffer_out.writeFloat(1.0) # Write object axis x
+			buffer_out.writeFloat(0) # Write object axis y
+			buffer_out.writeFloat(math.sin(time.monotonic() - initial_time)) # Write object axis z
+			buffer_out.writeFloat(time.monotonic() - initial_time + math.pi / 2.0) # Write object angle
+
+			buffer_out.updateLengthField()
+
+			buffer_out.writeToSocket(conn)
+		else:
+			time.sleep(0.01)
+
+
+
 
 
 
